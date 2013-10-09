4295fb1b1bbac7e78b66a0191331bc602440dfba
Ahmed ElSayed <ahmels@microsoft.com>
2013-10-09 15:29:23 -0700

    Perview WebAPI 5 app

---
+++ Preview_Webapi5/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Preview_Webapi5
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Preview_Webapi5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Preview_Webapi5.WebApiApplication" Language="C#" %>
